ab2873fad6d182aad612472fc9bdab7d6c1b5288
nikhil-lambda <nikhily@lambdatest.com>
2019-01-18 16:18:20 +0530

    env varibale support added.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,4 +1,14 @@
 module.exports = (function(settings) {
+  console.log(settings["test_settings"]["default"]["username"])
+
+  if (process.env.LAMBDATEST_USERNAME) {
+    settings["test_settings"]["default"]["username"] = process.env.LAMBDATEST_USERNAME;
+  }
+
+  if (process.env.LAMBDATEST_ACCESS_KEY) {
+    settings["test_settings"]["default"]["access_key"] = process.env.LAMBDATEST_ACCESS_KEY;
+  }
+
   if (process.env.SELENIUM_HOST) {
     settings.selenium.host = process.env.SELENIUM_HOST;
   }
