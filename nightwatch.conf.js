f92f9644ef7a98831c5e30e094d865509db1f91a
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2019-01-18 16:47:35 +0530

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,12 +1,12 @@
 module.exports = (function(settings) {
   console.log(settings["test_settings"]["default"]["username"])
 
-  if (process.env.LAMBDATEST_USERNAME) {
-    settings["test_settings"]["default"]["username"] = process.env.LAMBDATEST_USERNAME;
+  if (process.env.LT_USERNAME) {
+    settings["test_settings"]["default"]["username"] = process.env.LT_USERNAME;
   }
 
-  if (process.env.LAMBDATEST_ACCESS_KEY) {
-    settings["test_settings"]["default"]["access_key"] = process.env.LAMBDATEST_ACCESS_KEY;
+  if (process.env.LT_ACCESS_KEY) {
+    settings["test_settings"]["default"]["access_key"] = process.env.LT_ACCESS_KEY;
   }
 
   if (process.env.SELENIUM_HOST) {
