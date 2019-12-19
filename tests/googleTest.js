d38ab5c4ac3fe8fb4aa053301a1abeec938a77fe
prateekLambda <47247309+prateekLambda@users.noreply.github.com>
2019-12-19 18:10:30 +0530

    Marking test passed/failed on LambdaTest

--- tests/googleTest.js
+++ tests/googleTest.js
@@ -1,22 +1,48 @@
-var https = require('https');
-
+var https = require("https");
+var lambdaRestClient = require("@lambdatest/node-rest-client");
+var lambdaCredentials = {
+  username: process.env.LT_USERNAME,
+  accessKey: process.env.LT_ACCESS_KEY
+};
+var lambdaAutomationClient = lambdaRestClient.AutomationClient(
+  lambdaCredentials
+);
 module.exports = {
-
-    '@tags': ['test'],
-
-    'Google': function(client) {
-        client
-            .url('https://www.google.com/ncr')
-            .waitForElementVisible('body', 10000)
-            .setValue('input[type=text]', 'LambdaTest\n')
-            .pause(1000)
-            .assert.title('LambdaTest - Google Search')
-            .end();
-    },
-
-    afterEach: function(client, done) {
-        setTimeout(function() {
+  "@tags": ["test"],
+  Google: function(client) {
+    client
+      .url("https://www.google.com/ncr")
+      .waitForElementVisible("body", 10000)
+      .setValue("input[type=text]", "LambdaTest\n")
+      .pause(1000)
+      .assert.title("LambdaTest - Google Search")
+      .end();
+  },
+  after: function(browser) {
+    console.log("Closing down...");
+  },
+  afterEach: function(client, done) {
+    if (
+      process.env.LT_USERNAME &&
+      process.env.LT_ACCESS_KEY &&
+      client.capabilities &&
+      client.capabilities["webdriver.remote.sessionid"]
+    ) {
+      
+      lambdaAutomationClient.updateSessionById(
+        client.capabilities["webdriver.remote.sessionid"],
+        { status_ind: client.currentTest.results.failed ? "failed" : "passed" },
+        function(error, session) {
+          console.log(error)
+          if (!error) {
+            client.pause(10000)
             done();
-        }, 1000);
+          }
+        }
+      );
+    } else {
+      client.pause(10000)
+      done();
     }
+  }
 };
