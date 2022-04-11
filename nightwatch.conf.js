1ddde36775f4b6c0797ca9abc6350df4178597c4
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2022-04-11 21:26:21 +0000

    fixes

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,97 +1,80 @@
 
 
 const capabilities = {
-  'LT:Options' : {
-    "user" : '${LT_USERNAME}',
-    "accessKey" : '${LT_ACCESS_KEY}',
-    "build" : "your build name",
-    "name" : "your test name",
-    "platformName" : "Windows 10",
-    "selenium_version" : "4.1.2",
-    "seCdp" : "true"
+  'LT:Options': {
+    "user": '${LT_USERNAME}',
+    "accessKey": '${LT_ACCESS_KEY}',
+    "build": "your build name",
+    "platformName": "Windows 10",
+    "selenium_version": "4.1.2",
+    "seCdp": "true"
   }
 }
 
 const lambdatest = {
-  webdriver: {
-      start_process: false
-    },
-
-    selenium: {
-      host: 'hub.lambdatest.com',
-      port: 443
-    },
-
-    desiredCapabilities: {
-       browserName: 'chrome',
-      ...capabilities
-    }
+  selenium: {
+    host: 'hub.lambdatest.com',
+    port: 443
+  }
 }
 
 module.exports = {
   src_folders: [],
+
+  test_workers: {
+    enabled: false,
+    workers: 'auto',
+  },
+
+  webdriver: {
+    keep_alive: true,
+    timeout_options: {
+      timeout: 60000,
+      retry_attempts: 3
+    }
+  },
 
   test_settings: {
     default: {
       launch_url: 'https://nightwatchjs.org'
     },
 
-    lambdatest:  {
-        ...lambdatest
-    },
-
     "lambdatest.chrome": {
       ...lambdatest,
-      desiredCapabilities:{
-          browserName: 'chrome',
-          ...capabilities
+      desiredCapabilities: {
+        browserName: 'chrome',
+        ...capabilities
       }
     },
     "lambdatest.firefox": {
       ...lambdatest,
-      desiredCapabilities:{
-          browserName: 'firefox',
-          ...capabilities
+      desiredCapabilities: {
+        browserName: 'firefox',
+        platformName: 'windows 10',
+        ...capabilities
       }
     },
     "lambdatest.edge": {
       ...lambdatest,
-      desiredCapabilities:{
-          browserName: 'Edge',
-          ...capabilities
+      desiredCapabilities: {
+        browserName: 'Edge',
+        ...capabilities
       }
     },
   },
+  globals: {
 
-  beforeEach(browser, done)  {
-    // performing an async operation
-    console.log('before each');
-    setTimeout(function() {
-      // finished async duties
+
+    afterEach(browser, done) {
+
+      if (browser.currentTest.results.failed) {
+        browser.execute('lambda-status=failed');
+      } else if (browser.currentTest.results.errors) {
+        browser.execute('lambda-status=error');
+      } else {
+        browser.execute('lambda-status=passed');
+      }
       done();
-    }, 100);
-  },
-
-  afterEach(browser, done) {
-    // performing an async operation
-    console.log('after each');
-    setTimeout(function() {
-      // finished async duties
-      done();
-    }, 200);
-  },
-
-   // Called right after the command .navigateTo() is finished
-   async onBrowserNavigate(browser) {
-    console.log(browser.currentTest);
-    browser.execute('lambda-name='+browser.currentTest.name());
-    return Promise.resolve();
-  },
-
-  // Called right before the command .quite() is finished
-  async onBrowserQuit(browser) {
-    console.log('on quite');
-    return Promise.resolve();
+    }
   }
-
 };
